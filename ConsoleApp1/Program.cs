17666dce58f4309bf09dc744e67eb5fd40184eb8
Dmitriy <36476206+Onemoretitle@users.noreply.github.com>
2020-03-18 15:28:07 +0700

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class IProgram
+    {
+        public static int sumThis(int first, int second)
+        {
+            return first + second;
+        }
+
+        public static double divThis(double first, double second)
+        {
+            return first / second;
+        }
+
+        public static double multThis(double first, double second)
+        {
+            return first * second;
+        }
+
+        static void Main(string[] args)
+        {
+            
+            int a = sumThis(2, -3);
+            double b = divThis(17, 2);
+            double c = multThis(a, b);
+            string res = "a = " + a + "\nb = " + b + "\nc = " + c;
+            Console.WriteLine(res);
+        }
+    }
+}
